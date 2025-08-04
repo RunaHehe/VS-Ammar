be7b69c4ae79955e20c7ce3cc70766324bc36d63
Spook0w0 <spookupt@gmail.com>
2025-08-04 16:06:53 -0700

    isnt this supposed to be ignored

--- export/release/windows/bin/mods/custom_events/change camera speed.lua
+++
@@ -1,5 +0,0 @@
-function onEvent(name,v1,v2)
-    if name == 'change came speed' then
-        setProperty('cameraSpeed',v1)
-    end
-end
--- export/release/windows/bin/mods/stages/streets.lua
+++
@@ -1,6 +0,0 @@
-function onCreate()
-    setBlendMode('blendLighting', 'add')
-    setProperty('blendLighting.alpha', 0.65)
-end
-
---yeah this is the entire stage script LMAO
